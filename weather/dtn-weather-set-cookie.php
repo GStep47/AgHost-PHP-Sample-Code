852ed7056fa3f2da90caf1bc01d067ff56697ed6
Gary Stephen <angerthespy@gmail.com>
2016-08-02 13:42:07 -0500

    Create dtn-weather-set-cookie.php

---
+++ weather/dtn-weather-set-cookie.php
@@ -0,0 +1,19 @@
+<?php
+
+$enteredzip=$_POST['userzip']; 
+
+//sanitize entered data - remove everything except digits
+$enteredzip = filter_var($enteredzip,FILTER_SANITIZE_NUMBER_INT); 
+
+//sanitize entered data - if zip code is not long enough, set it to the default 99163
+if (strlen($enteredzip) <5) {
+	$enteredzip = "99163";
+}
+
+//set cookie
+setcookie ("WeatherZip", $enteredzip, time()+120000000, "/"); //about 3.8 years
+
+//redirect to index.php
+header("location:/../services/index.php");
+
+?>
